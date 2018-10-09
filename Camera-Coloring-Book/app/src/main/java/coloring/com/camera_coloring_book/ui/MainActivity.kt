de97d67b5fb37f11183593c932d69395971726b6
Woopa01 <whdnwls49@naver.com>
2018-10-09 22:38:12 +0900

    project setting

--- Camera-Coloring-Book/app/src/main/java/coloring/com/camera_coloring_book/ui/MainActivity.kt
+++ Camera-Coloring-Book/app/src/main/java/coloring/com/camera_coloring_book/ui/MainActivity.kt
@@ -1,10 +1,10 @@
-package coloring.com.camera_coloring_book
+package coloring.com.camera_coloring_book.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import coloring.com.camera_coloring_book.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
