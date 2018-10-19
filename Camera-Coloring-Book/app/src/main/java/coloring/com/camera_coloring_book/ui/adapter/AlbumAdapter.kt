77cae8a3cfa7c58efc0a5a16554f172141470781
Sungyong Son <space010127@gmail.com>
2018-10-20 01:07:27 +0900

    [Update] Img Loading Fast

--- Camera-Coloring-Book/app/src/main/java/coloring/com/camera_coloring_book/ui/adapter/AlbumAdapter.kt
+++ Camera-Coloring-Book/app/src/main/java/coloring/com/camera_coloring_book/ui/adapter/AlbumAdapter.kt
@@ -3,56 +3,51 @@ package coloring.com.camera_coloring_book.ui.adapter
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.net.Uri
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coloring.com.camera_coloring_book.R
 import kotlinx.android.synthetic.main.item_photo.view.*
-import java.io.FileNotFoundException
 
+class AlbumAdapter(private val context: Context,
+                   private val albumList: ArrayList<String>) : RecyclerView.Adapter<AlbumViewHolder>() {
 
-internal class AlbumAdapter(private val context: Context,
-                            private val albumList : ArrayList<String>) : RecyclerView.Adapter<AlbumViewHolder>() {
-
-    fun add(path : String){
-        albumList.add(path)
-        notifyDataSetChanged()
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder
-            = AlbumViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder = AlbumViewHolder(parent)
 
     override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-        holder.itemView.photo.setImageURI(Uri.parse(albumList[position]))
+        val img = decodeSampledBitmapFromURI(albumList[position], 200, 200)
+        if (img != null) holder.itemView.photo.setImageBitmap(img)
+        else holder.itemView.photo.setImageResource(R.drawable.ic_launcher_background)
     }
 
     override fun getItemCount() = albumList.size
 
-    private fun resize(context : Context, uri : Uri, resize: Int) : Bitmap? {
-        var resizeBitmap : Bitmap? = null
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
 
-        val options = BitmapFactory.Options()
-        try {
-            BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri), null, options) // 1번
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
 
-            var width = options.outWidth
-            var height = options.outHeight
-            var samplesize = 1
-
-            while (true) {//2번
-                if (width / 2 < resize || height / 2 < resize)
-                    break
-                width /= 2
-                height /= 2
-                samplesize *= 2
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
             }
-
-            options.inSampleSize = samplesize
-            val bitmap = BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri), null, options) //3번
-            resizeBitmap = bitmap
-
-        } catch (e : FileNotFoundException) {
-            e.printStackTrace()
         }
-        return resizeBitmap
+        return inSampleSize
+    }
+
+    private fun decodeSampledBitmapFromURI(
+            path: String,
+            reqWidth: Int,
+            reqHeight: Int
+    ): Bitmap? {
+        return BitmapFactory.Options().run {
+            inJustDecodeBounds = true
+            BitmapFactory.decodeFile(path, this)
+
+            inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+            inJustDecodeBounds = false
+            BitmapFactory.decodeFile(path, this)
+        }
     }
 }
--- Camera-Coloring-Book/app/src/main/java/coloring/com/camera_coloring_book/ui/palette/PaletteFragment.kt
+++ Camera-Coloring-Book/app/src/main/java/coloring/com/camera_coloring_book/ui/palette/PaletteFragment.kt
@@ -6,29 +6,30 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import coloring.com.camera_coloring_book.R
 import coloring.com.camera_coloring_book.model.ARGB
 import coloring.com.camera_coloring_book.ui.MainActivity
 import coloring.com.camera_coloring_book.ui.adapter.AlbumAdapter
 import coloring.com.camera_coloring_book.ui.adapter.PaletteAdapter
-import com.google.android.flexbox.*
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.fragment_palette.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class PaletteFragment : Fragment(){
 
     private lateinit var paletteAdapter : PaletteAdapter
     private lateinit var albumAdapter : AlbumAdapter
-    companion object {
+        companion object {
         lateinit var behavior: BottomSheetBehavior<ConstraintLayout>
     }
 
@@ -38,19 +39,12 @@ class PaletteFragment : Fragment(){
         behavior = BottomSheetBehavior.from(album_list_bottom_sheet)
 
         paletteAdapter = PaletteAdapter(context!!, 0, ArrayList())
-        albumAdapter = AlbumAdapter(context!!, ArrayList())
+        getAllImages((activity as MainActivity?)!!)
 
         val paletteLayoutManager
                 = FlexboxLayoutManager(context!!).apply {
             flexDirection = FlexDirection.ROW
             justifyContent = JustifyContent.CENTER
-        }
-
-        val albumLayoutManager
-                = FlexboxLayoutManager(context).apply {
-            flexWrap = FlexWrap.WRAP
-            flexDirection = FlexDirection.ROW
-            alignItems = AlignItems.CENTER
         }
 
         my_palette_recycler.apply {
@@ -60,25 +54,24 @@ class PaletteFragment : Fragment(){
 
         album_recycler.apply {
             adapter = albumAdapter
-            //layoutManager = albumLayoutManager
-            layoutManager = GridLayoutManager(context, 2)
+            layoutManager = GridLayoutManager(context, 3)
+            smoothScrollToPosition(0)
         }
-
         paletteAdapter.add(ARGB(255, 100, 140, 186))
         paletteAdapter.add(ARGB(255, 255, 64, 129))
         paletteAdapter.add(ARGB())
-
-        getAllShownImagesPath((activity as MainActivity?)!!)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_palette, container, false)
     }
 
-    private fun getAllShownImagesPath(activity: Activity){
+    @SuppressLint("Recycle")
+    private fun getAllImages(activity: Activity){
         val uri: Uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val cursor: Cursor?
         val columnIndexData: Int
+        val albumList = ArrayList<String>()
 
         val projection = arrayOf(MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
 
@@ -86,7 +79,10 @@ class PaletteFragment : Fragment(){
 
         columnIndexData = cursor!!.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
         while (cursor.moveToNext()) {
-            albumAdapter.add(cursor.getString(columnIndexData))
+            albumList.add(cursor.getString(columnIndexData))
         }
+        albumList.reverse()
+        albumAdapter = AlbumAdapter(context!!, albumList)
+        albumAdapter.notifyDataSetChanged()
     }
 }
